8fd880fd01cc81fecbb01617ddec3aa25720a81a
최주혜 <zue@kakao.com>
2021-05-09 19:30:43 +0900

    seeProfile 사용자 조회 기능 추가

--- src/users/users.queries.ts
+++ src/users/users.queries.ts
@@ -1,3 +1,13 @@
+import client from "../client";
+
+interface SeeProfileParams {
+  userName: string;
+}
+
 export default {
-  Query: {},
+  Query: {
+    seeProfile: (_: any, { userName }: SeeProfileParams) => {
+      return client.user.findUnique({ where: { userName } });
+    },
+  },
 };
