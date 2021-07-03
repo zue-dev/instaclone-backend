9bfd9ca51f82c2b5459e4c4ce4d95c66fd2a23d7
최주혜 <zue@kakao.com>
2021-07-03 14:58:25 +0900

    uploadPhoto resolvers

--- src/photos/uploadPhoto/uploadPhoto.resolvers.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -1,15 +1,36 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
     Mutation: {
         uploadPhoto: protectedResolver(async (_, { file, caption }, { loggedInUser }) => {
+            let hashtagObject = [];
+
             if (caption) {
                 // parse caption
                 // get or create hashtags
+                const hashtags = caption.match(/#[\w]+/g)
+                hashtagObject = hashtags.map((hashtag: any) => ({
+                    where: { hashtag },
+                    create: { hashtag }
+                }))
+
             }
 
             // save the photo with the parsed hashtags
             // add the photo to the hashtags
+            return client.photo.create({
+                data: {
+                    file,
+                    caption,
+                    user: {
+                        connect: { id: loggedInUser?.id }
+                    },
+                    ...(hashtagObject.length > 0 && {
+                        hashtags: { connectOrCreate: hashtagObject }
+                    })
+                }
+            })
         })
     }
 }
